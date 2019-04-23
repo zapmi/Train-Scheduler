85e7f026f0f9f401dd1f8c7f4dc482b932f88fe0
zapmi <mzapata2991@gmail.com>
2019-04-23 10:48:07 -0500

    more done to the app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,25 +13,25 @@ var database = firebase.database();
 
 $("#submit-bid").on("click", function (event) {
     event.preventDefault();
-    var empName = $("#inputName").val().trim();
-    var empRole = $("#inputDest").val().trim();
-    var empStart = $("#inputTime").val().trim();
+    var trainName = $("#inputName").val().trim();
+    var dest = $("#inputDest").val().trim();
+    var time = $("#inputTime").val().trim();
     // var monthsWork = $("inputWork").val().trim();;
-    var empRate = $("#inputFreq").val().trim();
+    var freq = $("#inputFreq").val().trim();
     // var totalBill = $("#inputBill").val().trim();
     console.log("test");
-    console.log(empName);
-    console.log(empRole);
-    console.log(empStart);
-    console.log(empRate);
+    console.log(trainName);
+    console.log(dest);
+    console.log(time);
+    console.log(freq);
 
 
     var employeeObj = {
-        name: empName,
-        role: empRole,
-        start: empStart,
+        name: trainName,
+        role: dest,
+        start: time,
         // work: monthsWork,
-        rate: empRate,
+        rate: freq,
         // billed: totalBill
     }
     database.ref().push(employeeObj);
@@ -41,13 +41,13 @@ $("#submit-bid").on("click", function (event) {
 
 database.ref().on("child_added", function (child) {
     console.log(child.val());
-    var empName = child.val().name;
-    var empRole = child.val().role;
-    var empStart = child.val().start;
-    var empRate = child.val().rate;
-    var momentInst = moment(empStart, 'MM/DD/YYYY');
+    var trainName = child.val().name;
+    var dest = child.val().role;
+    var time = child.val().start;
+    var freq = child.val().rate;
+    var momentInst = moment(time, 'MM/DD/YYYY');
     var monthsWork = momentInst.diff(moment(), 'months') * -1;
-    var totalBill = monthsWork * empRate;
+    var totalBill = monthsWork * freq;
 
 
 
@@ -56,11 +56,11 @@ database.ref().on("child_added", function (child) {
     //add row to body
 
     var today = moment().format('MMMM Do YYYY, h:mm:ss a');
-    var name = $("<td>").text(empName);
-    var role = $("<td>").text(empRole);
-    var start = $("<td>").text(empStart);
+    var name = $("<td>").text(trainName);
+    var role = $("<td>").text(dest);
+    var start = $("<td>").text(time);
     var work = $("<td>").text(monthsWork);
-    var rate = $("<td>").text(empRate);
+    var rate = $("<td>").text(freq);
     var billed = $("<td>").text(totalBill);
 
     var tRow = $("<tr>");
